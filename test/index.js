005accbdf7c4592a0a8379ac547c579a7b110c96
Nicolas Brassard <nitriques@users.noreply.github.com>
2018-03-26 11:43:10 -0400

    Add a unit test for the new trimmer

--- test/index.js
+++ test/index.js
@@ -4,3 +4,4 @@ require('tape-catch');
 require('tape').test.onFinish(() => process.exit(0));
 require('./sitemap');
 require('./parse');
+require('./process');
---
+++ test/process.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const _ = require('lodash');
+const process = require('../lib/process');
+const test = require('tape').test;
+const config = {
+	"maxRecordSize": 100
+};
+
+test('maxRecordSize', (t) => {
+	const rec = {
+		text: (new Array(100).fill('aaaaaaaaaa'))
+	};
+	const c = _.clone(config);
+	process.trimmer(rec, c);
+	t.equal(rec.text.length, 6);
+	t.end();
+});
