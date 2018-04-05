666e5f072922a8a36bfcea67e8228d45d3f96313
Nicolas Brassard <nitriques@users.noreply.github.com>
2018-04-05 15:54:54 -0400

    Exclude comments node since they are not content
    Fixes #32

--- test/parse.js
+++ test/parse.js
@@ -30,6 +30,27 @@ test('Simple parse', (t) => {
 	t.end();
 });
 
+test('Parse with html comments', (t) => {
+	const rec = {
+		date: now,
+		timestamp: now.getTime()
+	};
+	const c = _.cloneDeep(config);
+	c.selectors.push({key: 'p', selector: 'p'});
+	const data = `<html><head>
+	<title>test</title>
+	<head>
+	<body>
+		<p>This is the content <!-- This is a comment --></p>
+</body></html>`;
+	parse(rec, data, c);
+	t.equal(rec.date, now);
+	t.equal(rec.timestamp, now.getTime());
+	t.equal(rec.title, 'test');
+	t.equal(rec.p, 'This is the content');
+	t.end();
+});
+
 test('Custom selector parse', (t) => {
 	const rec = {};
 	const c = _.cloneDeep(config);
